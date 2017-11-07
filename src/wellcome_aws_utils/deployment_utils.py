e8dda9f98b08c312bd6835efa70c277d50b20ceb
Alex Chan <a.chan@wellcome.ac.uk>
2017-11-07 16:07:40 +0000

    Fix import name

--- src/wellcome_aws_utils/deployment_utils.py
+++ src/wellcome_aws_utils/deployment_utils.py
@@ -7,7 +7,7 @@ Shared library to help surface ECS deployment information.
 import collections
 import datetime
 
-from wellcome_lambda_utils.ecs_utils import (
+from wellcome_aws_utils.ecs_utils import (
     get_cluster_arns, get_service_arns, describe_service
 )
 
