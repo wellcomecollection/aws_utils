1fa910f1e21a0c2bb47f5076317ebc3d03a515c3
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2017-11-16 14:45:10 +0000

    address review comments

--- src/wellcome_aws_utils/dynamo_event.py
+++ src/wellcome_aws_utils/dynamo_event.py
@@ -2,11 +2,12 @@ from enum import Enum
 
 from boto3.dynamodb.types import TypeDeserializer
 
+from exceptions import UnWellcomeException
 
-class DynamoEventFactory:
-    @staticmethod
-    def create(event):
-        return [DynamoEvent(record) for record in event['Records']]
+
+def create_dynamo_events(event):
+    for record in event['Records']:
+        yield DynamoEvent(record)
 
 
 class DynamoEventType(Enum):
@@ -14,79 +15,32 @@ class DynamoEventType(Enum):
 
 
 class DynamoEvent:
-    def _sanity_check_record(self, record):
-        if not ('eventSource' in record):
-            raise Exception(f'Unrecognised event: {record}')
-
-        if record['eventSource'] != 'aws:dynamodb':
-            raise Exception(f'Event source is not aws:dynamodb: {record}')
-
     def _set_event_type(self, record):
-        if 'eventName' not in record:
-            raise Exception(f'No eventName found in {record}!')
-
-        self.event_type = None
         if self.record['eventName'] == 'REMOVE':
             self.event_type = DynamoEventType.REMOVE
-
-        if self.record['eventName'] == 'INSERT':
+        elif self.record['eventName'] == 'INSERT':
             self.event_type = DynamoEventType.INSERT
-
-        if self.record['eventName'] == 'MODIFY':
+        elif self.record['eventName'] == 'MODIFY':
             self.event_type = DynamoEventType.MODIFY
-
-        if self.event_type is None:
-            raise Exception(
+        else:
+            raise UnWellcomeException(
                 f'Unrecognised eventName found in {record}!'
             )
-
-    def _set_event_source_arn(self, record):
-        if 'eventSourceARN' not in record:
-            raise Exception(
-                f'No eventSourceARN attribute available on record: {record}'
-            )
-
-        self.event_source_arn = record['eventSourceARN']
-
-    def _extract_dynamodb_from_record(self, record):
-        if 'dynamodb' not in record:
-            raise Exception(
-                f'No dynamodb attribute available on record: {record}'
-            )
-
-        return record['dynamodb']
-
-    def _set_keys(self, dynamodb):
-        if 'Keys' not in dynamodb:
-            raise Exception(
-                f'No Keys attribute available on record.dynamodb: {dynamodb}'
-            )
-
-        self._keys = dynamodb['Keys']
-
-    def _set_images(self, dynamodb):
-        new_image = None
-        if 'NewImage' in dynamodb:
-            new_image = dynamodb['NewImage']
-
-        old_image = None
-        if 'OldImage' in dynamodb:
-            old_image = dynamodb['OldImage']
-
-        self._new_image = new_image
-        self._old_image = old_image
 
     def __init__(self, record):
         self.record = record
 
-        self._sanity_check_record(record)
-        self._set_event_type(record)
-        self._set_event_source_arn(record)
+        try:
+            self._set_event_type(record)
+            self.event_source_arn = record['eventSourceARN']
 
-        dynamodb = self._extract_dynamodb_from_record(record)
-
-        self._set_keys(dynamodb)
-        self._set_images(dynamodb)
+            self._keys = record['dynamodb']['Keys']
+            self._new_image = record['dynamodb'].get('NewImage')
+            self._old_image = record['dynamodb'].get('OldImage')
+        except KeyError as e:
+            raise UnWellcomeException(
+                f'{e} not found in {record}!'
+            )
 
     @staticmethod
     def _deserialize_values(image):
